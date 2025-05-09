30bb5b69f7b7b74f7b31c729b9c46d15ad5fea6a
romatvX_ <146443792+romansxxq@users.noreply.github.com>
2025-05-09 08:19:11 +0300

    pon

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Zavd3
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+
+            int n = 0;
+            if (!InputInt(ref n, "Введіть кількість елементів масиву N") || n <= 0)
+                return;
+
+            double[] array = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                if (!InputDouble(ref array[i], "Введіть елемент масиву X[" + i.ToString() + "]"))
+                    return;
+            }
+
+            // Сортуємо масив
+            Array.Sort(array);
+
+            double key = 0;
+            if (!InputDouble(ref key, "Введіть елемент для пошуку"))
+                return;
+
+            // Пошук усіх позицій
+            List<int> positions = FindAllPositions(array, key);
+
+            // Виведення
+            string res = "Відсортований масив:\n";
+            for (int i = 0; i < array.Length; i++)
+                res += "X[" + i.ToString() + "] = " + array[i].ToString() + "\n";
+
+            res += "\nРезультати пошуку:\n";
+            if (positions.Count > 0)
+                res += "Елемент " + key + " знайдено на позиціях: " + string.Join(", ", positions);
+            else
+                res += "Елемент " + key + " не знайдено в масиві.";
+
+            MessageBox.Show(res, "Результати", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        static bool InputInt(ref int x, string prompt)
+        {
+            string s = x.ToString();
+        retry:
+            s = Interaction.InputBox(prompt, "Ввід", s);
+            try
+            {
+                x = Convert.ToInt32(s);
+            }
+            catch
+            {
+                if (MessageBox.Show("Ви ввели неціле число. Повторити?", "Помилка", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    goto retry;
+                return false;
+            }
+            return true;
+        }
+
+        static bool InputDouble(ref double x, string prompt)
+        {
+            string s = x.ToString();
+        retry:
+            s = Interaction.InputBox(prompt, "Ввід", s);
+            try
+            {
+                x = Convert.ToDouble(s);
+            }
+            catch
+            {
+                if (MessageBox.Show("Ви ввели недійсне число. Повторити?", "Помилка", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    goto retry;
+                return false;
+            }
+            return true;
+        }
+
+        static List<int> FindAllPositions(double[] arr, double key)
+        {
+            List<int> result = new List<int>();
+            double tolerance = 1e-9;
+
+            int left = 0, right = arr.Length - 1;
+            int foundIndex = -1;
+
+            // Бінарний пошук
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (Math.Abs(arr[mid] - key) < tolerance)
+                {
+                    foundIndex = mid;
+                    break;
+                }
+                else if (arr[mid] < key)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+
+            // Якщо знайдено, обходимо всі входження
+            if (foundIndex != -1)
+            {
+                int i = foundIndex;
+                while (i >= 0 && Math.Abs(arr[i] - key) < tolerance)
+                {
+                    result.Add(i);
+                    i--;
+                }
+
+                i = foundIndex + 1;
+                while (i < arr.Length && Math.Abs(arr[i] - key) < tolerance)
+                {
+                    result.Add(i);
+                    i++;
+                }
+
+                result.Sort(); // Позиції у порядку зростання
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void generateArray(double[,] array, int N, int M, double a, double b)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < M; j++)
+                    array[i, j] = a + rnd.NextDouble() * (b - a);
+        }
+        static void printArray(double[,] array, int N, int M, string s)
+        {
+            Console.WriteLine(s);
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    Console.Write($"[{array[i, j]:F2}] \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        static double calculateProduct(double[,] array, int N, int M)
+        {
+            double product = 1;
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < M; j++)
+                    product *= array[i, j];
+            return product;
+
+        }
+        static void Main(string[] args)
+        {
+            double a = 18, b = 80;
+            int i, j;
+            Console.Write("Введіть кількість рядків: ");
+            int N = int.Parse(Console.ReadLine());
+            Console.Write("Введіть кількість стовпців: ");
+            int M = int.Parse(Console.ReadLine());
+            double[,] array = new double[N, M];
+            generateArray(array, N, M, a, b);
+            printArray(array, N, M, "Згенерований масив:");
+
+            double product = calculateProduct(array, N, M);
+            Console.Write($"Добуток елементів масиву: {product}");
+
+        }
+
+    }
+}
